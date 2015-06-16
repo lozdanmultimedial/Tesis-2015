f8a02456096bc7aaf69014b0f3f8feab4c476145
lozdanmultimedial <lozdanmultimedial@gmail.com>
2015-06-16 03:20:18 -0300

    Create bolas.h

---
+++ OF/Prototipos-Juego/Kosmosonico_fractal/bolas.h
@@ -0,0 +1,18 @@
+
+
+
+#ifndef _OF_BOLAS
+#define _OF_BOLAS
+#include "ofMain.h"
+
+class bolas
+{
+  public:
+      void actualizar();
+      void dib();
+      bolas();
+      float xx,yy,r,vX,vY, dirX, dirY;
+
+};
+
+#endif // _OF_BOLAS
