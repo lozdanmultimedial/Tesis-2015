13902a83a85a3e38a523161cd6365d9fc110f2c8
lozdanmultimedial <lozdanmultimedial@gmail.com>
2015-06-16 00:06:08 -0300

    Create bolas.h

---
+++ OF/Interfaz-sonica/ofxFFT/13_06_2015_FFT_Kosmico/bolas.h
@@ -0,0 +1,17 @@
+
+
+#ifndef _OF_BOLAS
+#define _OF_BOLAS
+#include "ofMain.h"
+
+class bolas
+{
+  public:
+      void actualizar();
+      void dib();
+      bolas();
+      float xx,yy,r,vX,vY, dirX, dirY;
+
+};
+
+#endif // _OF_BOLAS
