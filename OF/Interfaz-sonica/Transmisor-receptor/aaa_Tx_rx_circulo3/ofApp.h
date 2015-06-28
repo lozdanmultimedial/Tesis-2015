f0309a20003b7917b826412cf64547ad84a34f9a
lozdanmultimedial <lozdanmultimedial@gmail.com>
2015-06-27 22:55:59 -0300

    Update ofApp.h

--- OF/Interfaz-sonica/Transmisor-receptor/aaa_Tx_rx_circulo3/ofApp.h
+++ OF/Interfaz-sonica/Transmisor-receptor/aaa_Tx_rx_circulo3/ofApp.h
@@ -1,15 +1,15 @@
- 
  // este programa funciona en paraleo comunicandose enre si con "aaa_Rx_tx_Circulo3"
  //en ambos el fondo dinámico es generativo
  // aqui  hay un cubo cuyo movimiento se controla dándole su dirección con el puntero del mouse
  // y la acelaración con la intensidad de sonido capturado en tiempo real.
- // La data de las coordenadas x,y resultantes correspondientes al cubo son envidadas a "aaa_Rx_tx_Circulo3" para que allí 
+ // La data de las coordenadas x,y resultantes correspondientes al cubo son envidadas a "aaa_Rx_tx_Circulo3" para que allí
  // se grafique el mismo cubo.
  //
  //El otro porgrama envía aquí las oocrdenadas correspondiente a una esfera generada alli para ser graficada aqui.
- 
- 
- #pragma once
+
+
+
+#pragma once
 
 #include "ofMain.h"
 #include "ofxNetwork.h"
@@ -30,13 +30,6 @@ class ofApp : public ofBaseApp{
 
 		ofxUDPManager udpConnection;
 
-		 ofSoundStream soundStream;
-
-
-        void audioIn(float * input, int bufferSize, int nChannels);
-
-		vector <float> left;
-		vector <float> right;
-
-
 };
+
+ 
