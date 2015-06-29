57bd1a46915a82dc77f1a53d1923794b63af85ab
lozdanmultimedial <lozdanmultimedial@gmail.com>
2015-06-29 17:26:55 -0300

    Create ofApp.h

---
+++ OF/universos/aaa_prototipo_1a/ofApp.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "ofMain.h"
+#include "universo.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+         universo u;
+
+
+};
